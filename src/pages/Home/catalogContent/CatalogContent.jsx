96b411591c21e231e65679c14445a6bd0c38af49
Em Evgeniy <emevgeniy1323@gmail.com>
2023-12-26 12:52:00 +0500

    add fsd arc v1

--- src/pages/Home/catalogContent/CatalogContent.jsx
+++ src/pages/Home/catalogContent/CatalogContent.jsx
@@ -4,9 +4,12 @@ import classes from "./catalogContent.module.css";
 import Container from "../../../components/container/Container";
 import axios from "axios";
 import { useQuery } from "react-query";
+import { useSelector } from "react-redux";
+import FilterComp from "../../../components/UI/filterComp/FilterComp";
 
 const CatalogContent = () => {
 	const [data, setData] = useState([]);
+	const value2 = useSelector((state) => state.filter.value);
 	const { isSuccess } = useQuery({
 		queryKey: ["products"],
 		queryFn: async () => {
@@ -16,19 +19,47 @@ const CatalogContent = () => {
 			setData(data);
 		},
 	});
+
 	return (
 		<section className={classes.CatalogContent}>
 			{isSuccess && data?.length > 0 && (
 				<Container>
-					<div className={classes.inner}>
-						<Suspense fallback={<div>Loading...</div>}>
-							{data
-								?.filter((el) => el.category?.id !== 9)
-								.sort((a, b) => b.tags?.length - a.tags?.length)
-								.map((el, index) => (
-									<Card data={el} key={index} index={index} />
-								))}
-						</Suspense>
+					<div className={classes.wrap}>
+						<FilterComp />
+						<div className={classes.inner}>
+							<Suspense fallback={<div>Loading...</div>}>
+								{value2 === "По Цене(убыв)"
+									? data
+											?.filter((el) => el.category?.id !== 9)
+											.sort((a, b) => b.id - a.id)
+											.sort((a, b) => b.uzb_price - a.uzb_price)
+											.map((el, index) => (
+												<Card data={el} key={index} index={index} />
+											))
+									: value2 === "По Цене(возр)"
+									? data
+											?.filter((el) => el.category?.id !== 9)
+											.sort((a, b) => b.id - a.id)
+											.sort((a, b) => a.uzb_price - b.uzb_price)
+											.map((el, index) => (
+												<Card data={el} key={index} index={index} />
+											))
+									: value2 === "Популярные"
+									? data
+											?.filter((el) => el.category?.id !== 9)
+											.sort((a, b) => b.id - a.id)
+											.sort((a, b) => b.quantity - a.quantity)
+											.map((el, index) => (
+												<Card data={el} key={index} index={index} />
+											))
+									: data
+											?.filter((el) => el.category?.id !== 9)
+											.sort((a, b) => b.id - a.id)
+											.map((el, index) => (
+												<Card data={el} key={index} index={index} />
+											))}
+							</Suspense>
+						</div>
 					</div>
 				</Container>
 			)}
